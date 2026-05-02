1cf5c2f4869496d2df4213cc43058d02c4b0935a
glitterglue242 <brodybanster@gmail.com>
2026-05-02 23:44:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,38 @@ audio.onplay = () => {
 audio.onpause = () => {
   playBtn.style.transform = "scale(1)";
 };
+
+let lastX = 0;
+let lastY = 0;
+
+const cubes = [];
+const MAX = 25;
+
+document.addEventListener("mousemove", (e) => {
+  const dx = e.pageX - lastX;
+  const dy = e.pageY - lastY;
+
+  lastX = e.pageX;
+  lastY = e.pageY;
+
+  // position cube slightly BEHIND cursor movement
+  const cube = document.createElement("div");
+  cube.className = "trail-box";
+
+  cube.style.left = (e.pageX - dx * 0.6) + "px";
+  cube.style.top = (e.pageY - dy * 0.6) + "px";
+
+  document.body.appendChild(cube);
+  cubes.push(cube);
+
+  if (cubes.length > MAX) {
+    cubes.shift().remove();
+  }
+
+  setTimeout(() => {
+    cube.style.opacity = "0";
+    cube.style.transform = "scale(0.3)";
+  }, 40);
+
+  setTimeout(() => cube.remove(), 300);
+});
